ebab301b51215c6c13e32b83df084223d38c563b
Антон <sierra_93@mail.ru>
2022-03-16 21:42:29 +0300

    Настроил сваггер и конфиги для сервера.

--- Leoka.Elementary.Platform.Backend/Program.cs
+++ Leoka.Elementary.Platform.Backend/Program.cs
@@ -1,25 +1,33 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddControllersAsServices();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Leoka.Elementary.Platform", Version = "v1" });
+});
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(9991);  // Для http.
+    // options.ListenAnyIP(9990, configure => configure.UseHttps());    // Для https.
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseDeveloperExceptionPage();
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapDefaultControllerRoute();
+});
 
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Leoka.Elementary.Platform"));
 app.Run();
